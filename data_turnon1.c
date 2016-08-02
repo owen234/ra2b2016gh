63b25466f5999afb13cf0e745d4e9e6cf0d2757a
Amin Ghiasi <aghiasi@strange.ucr.edu>
2016-08-02 15:09:26 -0700

    Changing doSkimSlim.c and data_turnon1.c to add Njets = 2 option

--- data_turnon1.c
+++ data_turnon1.c
@@ -1,3 +1,8 @@
+#include "TChain.h"
+#include "TStyle.h"
+#include "TPad.h"
+#include "TSystem.h"
+#include <string>
 
 #include "histio.c"
 
@@ -6,7 +11,11 @@
 
   //---------
 
-   void data_turnon1() {
+   void data_turnon1( bool include_Njets2 = true ) {
+
+      TString Njet_cut;
+      if ( include_Njets2 == true ) Njet_cut = "& NJets >= 2";
+      else                           Njet_cut = "& NJets >= 3";
 
       TChain ch_ht("tree") ;
       TChain ch_met("tree") ;
@@ -20,60 +29,56 @@
       ///////ch_met.Add("fnal-prod-v9-skims-slimmed/tree_LDP/tree_MET_2016B-slimskim.root") ;
       ///////ch_met.Add("fnal-prod-v9-skims-slimmed/tree_LDP/tree_MET_2016C-slimskim.root") ;
      //-----------
-      ch_ht.Add("fnal-prod-v9-skims-slimmed/tree_LDP_july21a/tree_JetHT_2016B-slimskim.root") ;
-      ch_ht.Add("fnal-prod-v9-skims-slimmed/tree_LDP_july21a/tree_JetHT_2016C-slimskim.root") ;
-      ch_ht.Add("fnal-prod-v9-skims-slimmed/tree_LDP_july21a/tree_JetHT_2016D-slimskim.root") ;
-      ch_met.Add("fnal-prod-v9-skims-slimmed/tree_LDP_july21a/tree_MET_2016B-slimskim.root") ;
-      ch_met.Add("fnal-prod-v9-skims-slimmed/tree_LDP_july21a/tree_MET_2016C-slimskim.root") ;
-      ch_met.Add("fnal-prod-v9-skims-slimmed/tree_LDP_july21a/tree_MET_2016D-slimskim.root") ;
+      ch_ht.Add("fnal-prod-v9-skims-slimmed/tree_LDP/tree_JetHT_2016B-slimskim.root") ;
+      ch_ht.Add("fnal-prod-v9-skims-slimmed/tree_LDP/tree_JetHT_2016C-slimskim.root") ;
+      ch_ht.Add("fnal-prod-v9-skims-slimmed/tree_LDP/tree_JetHT_2016D-slimskim.root") ;
+      ch_met.Add("fnal-prod-v9-skims-slimmed/tree_LDP/tree_MET_2016B-slimskim.root") ;
+      ch_met.Add("fnal-prod-v9-skims-slimmed/tree_LDP/tree_MET_2016C-slimskim.root") ;
+      ch_met.Add("fnal-prod-v9-skims-slimmed/tree_LDP/tree_MET_2016D-slimskim.root") ;
      //-----------
 
       //int nbin(35) ;
       //float xl(0.) ;
       //float xh(700.) ;
 
+      
+      int bin_edges_nj[100];
+      int bi,nb_nj ;
+
+      bi = 0 ;
+      if ( include_Njets2 == true )
+      {   bin_edges_nj[bi] = 2 ; bi++ ;}
+      bin_edges_nj[bi] = 3 ; bi++ ;
+      bin_edges_nj[bi] = 5 ; bi++ ;
+      bin_edges_nj[bi] = 7 ; bi++ ;
+      bin_edges_nj[bi] = 9 ; bi++ ;
+      bin_edges_nj[bi] = 100 ;
+      nb_nj = bi ;
+
+
       int nbin(10) ;
       double xbins[11] = {0., 200., 220, 240, 260, 280, 300, 340, 400, 500, 700 } ;
 
-      TH1F* h_mht_nj1_denom = new TH1F( "h_mht_nj1_denom", "MHT, Nj1, HT800" ,  nbin, xbins ) ;
-      TH1F* h_mht_nj1_numer = new TH1F( "h_mht_nj1_numer", "MHT, Nj1, MET100",  nbin, xbins ) ;
-      TH1F* h_eff_nj1 = new TH1F( "h_eff_nj1", "Eff, Nj1", nbin, xbins ) ;
+      TH1F* denom_h_mht_nj[10];
+      TH1F* numer_h_mht_nj[10];
+      TH1F* h_eff_nj[10];
 
-      TH1F* h_mht_nj2_denom = new TH1F( "h_mht_nj2_denom", "MHT, nj2, HT800" ,  nbin, xbins ) ;
-      TH1F* h_mht_nj2_numer = new TH1F( "h_mht_nj2_numer", "MHT, nj2, MET100",  nbin, xbins ) ;
-      TH1F* h_eff_nj2 = new TH1F( "h_eff_nj2", "Eff, nj2", nbin, xbins ) ;
+      for ( int nj = 0; nj < nb_nj; nj++)
+      {
 
-      TH1F* h_mht_nj3_denom = new TH1F( "h_mht_nj3_denom", "MHT, nj3, HT800" ,  nbin, xbins ) ;
-      TH1F* h_mht_nj3_numer = new TH1F( "h_mht_nj3_numer", "MHT, nj3, MET100",  nbin, xbins ) ;
-      TH1F* h_eff_nj3 = new TH1F( "h_eff_nj3", "Eff, nj3", nbin, xbins ) ;
+      TString nj_str     ; nj_str     .Form("%d",nj+1); 
+      TString nj_cut_low ; nj_cut_low .Form("%d",bin_edges_nj[nj]);
+      TString nj_cut_high; nj_cut_high.Form("%d",bin_edges_nj[nj+1]);
 
-      TH1F* h_mht_nj4_denom = new TH1F( "h_mht_nj4_denom", "MHT, nj4, HT800" ,  nbin, xbins ) ;
-      TH1F* h_mht_nj4_numer = new TH1F( "h_mht_nj4_numer", "MHT, nj4, MET100",  nbin, xbins ) ;
-      TH1F* h_eff_nj4 = new TH1F( "h_eff_nj4", "Eff, nj4", nbin, xbins ) ;
+      denom_h_mht_nj[nj] = new TH1F( "h_mht_nj"+nj_str+"_denom", "MHT, Nj"+nj_str+", HT800" ,  nbin, xbins ) ;
+      numer_h_mht_nj[nj] = new TH1F( "h_mht_nj"+nj_str+"_numer", "MHT, Nj"+nj_str+", MET100",  nbin, xbins ) ;
+      h_eff_nj[nj]       = new TH1F( "h_eff_nj"+nj_str, "Eff, Nj"+nj_str, nbin, xbins ) ;
 
+      ch_ht.Draw("MHT>>h_mht_nj"+nj_str+"_denom","HBHEIsoNoiseFilter == 1 && HBHENoiseFilter == 1 && eeBadScFilter == 1 && EcalDeadCellTriggerPrimitiveFilter == 1 && NVtx > 0 && JetID &&  HT>900 && (MET>100 && CaloMET>80 && MET/CaloMET<5) && pass_ht800_trig && NJets >= "+ nj_cut_low + " && NJets < " + nj_cut_high) ;
+      ch_met.Draw("MHT>>h_mht_nj"+nj_str+"_numer", "HBHEIsoNoiseFilter == 1 && HBHENoiseFilter == 1 && eeBadScFilter == 1 && EcalDeadCellTriggerPrimitiveFilter == 1 && NVtx > 0 && JetID && HT>900 && (MET>100 && CaloMET>80 && MET/CaloMET<5) && pass_ht800_trig && (pass_pfmet100_trig||pass_pfmetnomu100_trig) && NJets >= "+ nj_cut_low + " && NJets < " + nj_cut_high ) ;
 
-      ch_ht.Draw("MHT>>h_mht_nj1_denom","HBHEIsoNoiseFilter == 1 && HBHENoiseFilter == 1 && eeBadScFilter == 1 && EcalDeadCellTriggerPrimitiveFilter == 1 && NVtx > 0 && JetID && NJets>=3 && NJets<=4 && HT>900 && (MET>100 && CaloMET>80 && MET/CaloMET<5) && pass_ht800_trig" ) ;
-      ch_met.Draw("MHT>>h_mht_nj1_numer", "HBHEIsoNoiseFilter == 1 && HBHENoiseFilter == 1 && eeBadScFilter == 1 && EcalDeadCellTriggerPrimitiveFilter == 1 && NVtx > 0 && JetID && NJets>=3 && NJets<=4 && HT>900 && (MET>100 && CaloMET>80 && MET/CaloMET<5) && pass_ht800_trig && (pass_pfmet100_trig||pass_pfmetnomu100_trig)" ) ;
-
-      ch_ht.Draw("MHT>>h_mht_nj2_denom","HBHEIsoNoiseFilter == 1 && HBHENoiseFilter == 1 && eeBadScFilter == 1 && EcalDeadCellTriggerPrimitiveFilter == 1 && NVtx > 0 && JetID && NJets>=5 && NJets<=6 && HT>900 && (MET>100 && CaloMET>80 && MET/CaloMET<5) && pass_ht800_trig" ) ;
-      ch_met.Draw("MHT>>h_mht_nj2_numer", "HBHEIsoNoiseFilter == 1 && HBHENoiseFilter == 1 && eeBadScFilter == 1 && EcalDeadCellTriggerPrimitiveFilter == 1 && NVtx > 0 && JetID && NJets>=5 && NJets<=6 && HT>900 && (MET>100 && CaloMET>80 && MET/CaloMET<5) && pass_ht800_trig && (pass_pfmet100_trig||pass_pfmetnomu100_trig)" ) ;
-
-      ch_ht.Draw("MHT>>h_mht_nj3_denom","HBHEIsoNoiseFilter == 1 && HBHENoiseFilter == 1 && eeBadScFilter == 1 && EcalDeadCellTriggerPrimitiveFilter == 1 && NVtx > 0 && JetID && NJets>=7 && NJets<=8 && HT>900 && (MET>100 && CaloMET>80 && MET/CaloMET<5) && pass_ht800_trig" ) ;
-      ch_met.Draw("MHT>>h_mht_nj3_numer", "HBHEIsoNoiseFilter == 1 && HBHENoiseFilter == 1 && eeBadScFilter == 1 && EcalDeadCellTriggerPrimitiveFilter == 1 && NVtx > 0 && JetID && NJets>=7 && NJets<=8 && HT>900 && (MET>100 && CaloMET>80 && MET/CaloMET<5) && pass_ht800_trig && (pass_pfmet100_trig||pass_pfmetnomu100_trig)" ) ;
-
-      ch_ht.Draw("MHT>>h_mht_nj4_denom","HBHEIsoNoiseFilter == 1 && HBHENoiseFilter == 1 && eeBadScFilter == 1 && EcalDeadCellTriggerPrimitiveFilter == 1 && NVtx > 0 && JetID && NJets>=9 && HT>900 && (MET>100 && CaloMET>80 && MET/CaloMET<5) && pass_ht800_trig" ) ;
-      ch_met.Draw("MHT>>h_mht_nj4_numer", "HBHEIsoNoiseFilter == 1 && HBHENoiseFilter == 1 && eeBadScFilter == 1 && EcalDeadCellTriggerPrimitiveFilter == 1 && NVtx > 0 && JetID && NJets>=9 && HT>900 && (MET>100 && CaloMET>80 && MET/CaloMET<5) && pass_ht800_trig && (pass_pfmet100_trig||pass_pfmetnomu100_trig)" ) ;
-
-      add_overflow( h_mht_nj1_denom ) ;
-      add_overflow( h_mht_nj2_denom ) ;
-      add_overflow( h_mht_nj3_denom ) ;
-      add_overflow( h_mht_nj4_denom ) ;
-
-      add_overflow( h_mht_nj1_numer ) ;
-      add_overflow( h_mht_nj2_numer ) ;
-      add_overflow( h_mht_nj3_numer ) ;
-      add_overflow( h_mht_nj4_numer ) ;
-
+      add_overflow( denom_h_mht_nj[nj] ) ;
+      add_overflow( numer_h_mht_nj[nj] ) ;
 
       for ( int bi=1; bi<=nbin; bi++ ) {
 
@@ -81,77 +86,33 @@
          float eff_err(0.) ;
          float denom, numer ;
 
-         denom = h_mht_nj1_denom -> GetBinContent( bi ) ;
-         numer = h_mht_nj1_numer -> GetBinContent( bi ) ;
+         denom = denom_h_mht_nj[nj] -> GetBinContent( bi ) ;
+         numer = numer_h_mht_nj[nj] -> GetBinContent( bi ) ;
          if (denom>0) {
             eff_val = numer/denom ;
             eff_err = sqrt( eff_val*(1.-eff_val)/denom ) ;
          }
-         h_eff_nj1 -> SetBinContent( bi, eff_val ) ;
-         h_eff_nj1 -> SetBinError( bi, eff_err ) ;
+         h_eff_nj[nj] -> SetBinContent( bi, eff_val ) ;
+         h_eff_nj[nj] -> SetBinError( bi, eff_err ) ;
 
-
-         denom = h_mht_nj2_denom -> GetBinContent( bi ) ;
-         numer = h_mht_nj2_numer -> GetBinContent( bi ) ;
-         if (denom>0) {
-            eff_val = numer/denom ;
-            eff_err = sqrt( eff_val*(1.-eff_val)/denom ) ;
-         }
-         h_eff_nj2 -> SetBinContent( bi, eff_val ) ;
-         h_eff_nj2 -> SetBinError( bi, eff_err ) ;
-
-
-         denom = h_mht_nj3_denom -> GetBinContent( bi ) ;
-         numer = h_mht_nj3_numer -> GetBinContent( bi ) ;
-         if (denom>0) {
-            eff_val = numer/denom ;
-            eff_err = sqrt( eff_val*(1.-eff_val)/denom ) ;
-         }
-         h_eff_nj3 -> SetBinContent( bi, eff_val ) ;
-         h_eff_nj3 -> SetBinError( bi, eff_err ) ;
-
-
-         denom = h_mht_nj4_denom -> GetBinContent( bi ) ;
-         numer = h_mht_nj4_numer -> GetBinContent( bi ) ;
-         if (denom>0) {
-            eff_val = numer/denom ;
-            eff_err = sqrt( eff_val*(1.-eff_val)/denom ) ;
-         }
-         h_eff_nj4 -> SetBinContent( bi, eff_val ) ;
-         h_eff_nj4 -> SetBinError( bi, eff_err ) ;
 
       } // bi
 
       gStyle -> SetOptStat(0) ;
 
-      h_eff_nj1 -> SetMaximum( 1.05 ) ;
-      h_eff_nj1 -> SetMinimum( 0.5 ) ;
+      h_eff_nj[nj] -> SetMaximum( 1.05 ) ;
+      h_eff_nj[nj] -> SetMinimum( 0.5 ) ;
 
-      h_eff_nj2 -> SetMaximum( 1.05 ) ;
-      h_eff_nj2 -> SetMinimum( 0.5 ) ;
+      h_eff_nj[nj] -> SetMarkerStyle(20+nj) ;
 
-      h_eff_nj3 -> SetMaximum( 1.05 ) ;
-      h_eff_nj3 -> SetMinimum( 0.5 ) ;
-
-      h_eff_nj4 -> SetMaximum( 1.05 ) ;
-      h_eff_nj4 -> SetMinimum( 0.5 ) ;
-
-
-      h_eff_nj1 -> SetMarkerStyle(20) ;
-      h_eff_nj2 -> SetMarkerStyle(21) ;
-      h_eff_nj3 -> SetMarkerStyle(22) ;
-      h_eff_nj4 -> SetMarkerStyle(23) ;
-
-      h_eff_nj2 -> SetMarkerColor(2) ;
-      h_eff_nj3 -> SetMarkerColor(3) ;
-      h_eff_nj4 -> SetMarkerColor(4) ;
-
-      h_eff_nj1 -> Draw() ;
+      h_eff_nj[nj] -> SetMarkerColor(2+nj) ;
+}//nj
+      h_eff_nj[0] -> Draw() ;
       gPad -> SetGridx(1) ;
       gPad -> SetGridy(1) ;
-      h_eff_nj2 -> Draw("same") ;
-      h_eff_nj3 -> Draw("same") ;
-      h_eff_nj4 -> Draw("same") ;
+      h_eff_nj[1] -> Draw("same") ;
+      h_eff_nj[2] -> Draw("same") ;
+      h_eff_nj[3] -> Draw("same") ;
 
 
       gSystem -> Exec( "mkdir -p outputfiles" ) ;
