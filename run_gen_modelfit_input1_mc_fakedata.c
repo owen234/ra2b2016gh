58bfa1ce192ecb3b0288dd15efedaa45f9ddf3ca
Owen <owen234@gmail.com>
2016-10-18 10:11:46 -0700

    Code to prepare fake input from MC

---
+++ run_gen_modelfit_input1_mc_fakedata.c
@@ -0,0 +1,17 @@
+
+#include "gen_modelfit_input1.c"
+
+   void run_gen_modelfit_input1_mc_fakedata() {
+
+      gSystem -> Exec( "mkdir -p outputfiles/files-modelfit-input-mc-fakedata" ) ;
+
+      gen_modelfit_input1(  "outputfiles/mc-nbsum-input-fakedata.txt",
+                            "outputfiles/mc-nbsum-input-lostlep.txt",
+                            "outputfiles/mc-nbsum-input-hadtau.txt",
+                            "outputfiles/mc-nbsum-input-znunu.txt",
+                            "outputfiles/modelfit-input-mc-fakedata.root" ) ;
+
+      gSystem -> Exec("mv outputfiles/modelfit-input*.* outputfiles/files-modelfit-input-mc-fakedata" ) ;
+
+   }
+
